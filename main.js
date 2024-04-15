b045a7a7f0643f2fbb4e2448e95b01c439586aa3
ALDono <aubreyldono@gmail.com>
2024-04-15 13:18:20 +0100

    modified:   main.js 	modified:   packageenquiry.html 	modified:   style.css

--- main.js
+++ main.js
@@ -20,27 +20,43 @@ const navToggle = document.querySelector("#navToggle");
 
 const subBtn = document.querySelector("#subBtn");
 const otpBtn = document.querySelector("#otpBtn");
+const addonBtn = document.querySelector("#addonBtn");
 const otpPriceCard = document.querySelector("#otpPriceCard");
 const subPriceCard = document.querySelector("#subPriceCard");
+const addonCard = document.querySelector("#addonCard");
 const closePackageInfo1 = document.querySelector("#closePackageInfo1");
 const closePackageInfo2 = document.querySelector("#closePackageInfo2");
+const closePackageInfo3 = document.querySelector("#closePackageInfo3");
 
 subBtn.addEventListener("click",function(){
-  subPriceCard.style.display = "block";
+  subPriceCard.style.display = "flex";
 });
 
 otpBtn.addEventListener("click",function(){
-  otpPriceCard.style.display = "block";
+  otpPriceCard.style.display = "flex";
+});
+
+addonBtn.addEventListener("click",function(){
+  addonCard.style.display = "flex";
 });
 
 closePackageInfo1.addEventListener("click",function(){
   otpPriceCard.style.display = "none";
   subPriceCard.style.display = "none";
+  addonCard.style.display = "none";
 })
 
 closePackageInfo2.addEventListener("click",function(){
   otpPriceCard.style.display = "none";
   subPriceCard.style.display = "none";
+  addonCard.style.display = "none";
+})
+
+closePackageInfo3.addEventListener("click",function(){
+  otpPriceCard.style.display = "none";
+  subPriceCard.style.display = "none";
+  addonCard.style.display = "none";
+
 })
 
 
